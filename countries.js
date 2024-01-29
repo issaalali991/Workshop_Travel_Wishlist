0d829b6f4b80309dbc5a15871e67b5a0893bddc7
issaali <issa.alali.991@gmail.com>
2024-01-29 12:20:50 +0100

    first

---
+++ countries.js
@@ -0,0 +1,36 @@
+const countries = [
+  {
+    id:1,
+    name:"France",
+    alpha2Code: "FR",
+    alpha3Code: "FRA",
+  },
+  {
+    id:2,
+    name: "Åland Islands",
+    alpha2Code: "AX",
+    alpha3Code: "ALA",
+  },
+  {
+    id:3,
+    name: "Albania",
+    alpha2Code: "AL",
+    alpha3Code: "ALB",
+  },
+  {
+    id:4,
+    name: "Algeria",
+    alpha2Code: "DZ",
+    alpha3Code: "DZA",
+  },
+  {
+    id:5,
+    name: "American Samoa",
+    alpha2Code: "AS",
+    alpha3Code: "ASM",
+  },
+ 
+  
+];
+  
+export default countries;
